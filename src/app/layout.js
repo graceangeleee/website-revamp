fdb63a1cfa0f82e995320754ea1fda75fcf2e245
graceangeleee <gdemdem@up.edu.ph>
2023-08-11 15:31:53 +0800

    modify: drawer, placeholder background

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-home bg-cover bg-no-repeat'>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <main className="flex relative min-h-screen flex-col justify-between p-5">
         <Header/>
-        <h1 className="relative self-center font-poppins font-bold">UPLB Computer Science Society</h1>
-        {/* uncomment when bg is added
-        <h1 className="relative self-center font-poppins font-bold text-white">UPLB Computer Science Society</h1> */}
+        {/* <h1 className="relative self-center font-poppins font-bold">UPLB Computer Science Society</h1> */}
+        {/* uncomment when bg is added*/}
+        <h1 className="relative self-center font-poppins font-bold text-white">UPLB Computer Science Society</h1>
         <div className="flex flex-row">
-          <Sidebar/>
+        <Sidebar/>
         </div>
-       
+        
     </main>
   )
 }
