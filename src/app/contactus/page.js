a7147fe7ed6ab7c3fb6fe9b7a60a34f864be880f
graceangeleee <gdemdem@up.edu.ph>
2023-09-02 12:10:12 +0800

    added header

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -1,7 +1,12 @@
+import Header from "../header"
+
 export default function Contact(){
     return(
         <div className='bg-home bg-cover bg-no-repeat'>
+            <Header/>
+            <div className="flex flex-row">
 
+            </div>
         </div>
     )
 }
