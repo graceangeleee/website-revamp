2f2c333a803670750f8c67791d039345dd47cc51
graceangeleee <gdemdem@up.edu.ph>
2023-09-02 22:12:45 +0800

    forms and contact details

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -1,11 +1,16 @@
 import Header from "../header"
+import Form from "./form"
+import ContactDetails from "./contactDetails"
 
 export default function Contact(){
     return(
-        <div className='bg-home bg-cover bg-no-repeat'>
+        <div className='flex flex-col bg-home bg-cover bg-no-repeat'>
             <Header/>
-            <div className="flex flex-row">
-
+            <div className="flex flex-row font-poppins text-white">
+                <div className="flex flex-col">
+                    <Form/>
+                    <ContactDetails/>
+                </div>     
             </div>
         </div>
     )
