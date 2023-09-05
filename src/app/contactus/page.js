dad99ed47100abdd8d002251ef11c856ffc21403
jcsestilo <jsestilo@up.edu.ph>
2023-09-05 21:35:24 +0800

    started creating dale get in touch

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -1,12 +1,13 @@
-import Header from "../header"
+import Header from '../header';
+import GetInTouch from './getintouch';
 
-export default function Contact(){
-    return(
-        <div className='bg-home bg-cover bg-no-repeat'>
-            <Header/>
-            <div className="flex flex-row">
-
-            </div>
-        </div>
-    )
+export default function Contact() {
+  return (
+    <div className='bg-home bg-cover bg-no-repeat'>
+      <Header />
+      <div className='flex flex-row'>
+        <GetInTouch />
+      </div>
+    </div>
+  );
 }
