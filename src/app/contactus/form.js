db3421297694128a22dbc4fc51511ad72e59f89d
graceangeleee <gdemdem@up.edu.ph>
2023-09-07 20:00:24 +0800

    fix: input on focus

--- src/app/contactus/form.js
+++ src/app/contactus/form.js
@@ -6,34 +6,34 @@ export default function Form() {
               <input
                 type="text"
                 placeholder="first name"
-                className="border-b-1 border-white bg-blue-950 focus:border-none mr-10 py-3"
+                className="border-b-1 border-white bg-blue-950 focus:outline-none mr-10 py-3"
               />
               <input
                 type="text"
                 placeholder="last name"
-                className="border-b-1 border-white bg-blue-950 focus:border-none py-3"
+                className="border-b-1 border-white bg-blue-950 focus:outline-none py-3"
               />
             </div>
             <div className="flex flex-row mt-5">
               <input
                 type="text"
                 placeholder="phone number"
-                className="border-b-1 border-white bg-blue-950 focus:border-none mr-10 py-3"
+                className="border-b-1 border-white bg-blue-950 focus:outline-none mr-10 py-3"
               />
               <input
                 type="text"
                 placeholder="email"
-                className="border-b-1 border-white bg-blue-950 focus:border-none py-3"
+                className="border-b-1 border-white bg-blue-950 focus:outline-none py-3"
               />
             </div>
             <input
               type="text"
               placeholder="group, organization, or company"
-              className="border-b-1 border-white bg-blue-950 focus:border-none mt-5 py-3"
+              className="border-b-1 border-white bg-blue-950 focus:outline-none mt-5 py-3"
             />
             <textarea
               rows={5}
-              className="border-b-1 border-white bg-blue-950 focus:border-none mt-5"
+              className="border-b-1 border-white bg-blue-950 focus:outline-none mt-5"
               placeholder="how can we help you?"
             ></textarea>
             <button type="Submit" className="mt-5 border-1 py-3 px-10 rounded-md max-w-md self-center bg-blue-button mb-5">Submit</button>
